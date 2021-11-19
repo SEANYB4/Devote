712e8b414c4cbf67f12a2f71e9b287c05c4c6e83
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-11-19 20:29:50 +0000

    Completed the project.

---
+++ Devote/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  Devote
+//
+//  Created by Sean Bain on 18/11/2021.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
